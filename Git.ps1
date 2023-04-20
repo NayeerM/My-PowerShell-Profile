60f83eb3c9a0080ec0c1406eb69317cf96db7640
Nayeer Bin Mahiuddin <nayeer@tessinternational.com>
2023-04-20 15:03:31 +0800

    added new functions

--- Git.ps1
+++ Git.ps1
@@ -1,5 +1,18 @@
+function Stash {
+    param (
+        [Parameter(Mandatory=$true, Position=0)]
+        [ValidateSet("pop", "push")]
+        [string]$Config
+    )
 
+    if ($Config -eq "pop") {
+        $stashList = git stash list
+        $stashToApply = $stashList | Select-String "Excluded Files"
+        $stashIndex = $stashToApply.Line.Split(':')[0]
 
-function Log () {
-    git log
+        git stash pop $stashIndex
+    }
+    elseif ($Config -eq "push") {
+        & git stash push -m "Excluded Files" -- CORAL-iSEM-v10-BE/appsettings.json
+    }
 }
--- Profile.ps1
+++ Profile.ps1
@@ -1,14 +1,41 @@
 
 
 oh-my-posh init pwsh --config 'C:\Users\nayeer\AppData\Local\Programs\oh-my-posh\themes\m365princess.omp.json' | Invoke-Expression
-
 # Import modules
- . Get-Location\Git.ps1
+ . 'D:\MyPowerShellProfile\Git.ps1'
 
-function ChangeBackend {
-    Set-Location 'D:\ISEM10 v2\CORAL-iSEM-BE'
+# PSReadline Settings
+Set-PSReadlineOption -TokenKind Command -ForegroundColor Green
+Set-PSReadlineKeyHandler -Key Tab -Function Complete
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadlineOption -EditMode Windows
+
+function Set-Folder {
+    param (
+        [Parameter(Mandatory=$true, Position=0)]
+        [ValidateSet("f", "b")]
+        [string]$Target
+    )
+    
+    if ($Target -eq "f") {
+        Set-Location 'D:\ISEM10 v2\CORAL iSEM10 v2'
+    }
+    elseif ($Target -eq "b") {
+        Set-Location 'D:\ISEM10 v2\CORAL-iSEM-BE'
+    }
 }
 
-function ChangeFrontend{
-    Set-Location 'D:\ISEM10 v2\CORAL iSEM10 v2'
+function Touch {
+    param (
+        [Parameter(Mandatory=$true, Position=0)]
+        [string]$Path
+    )
+    if (-not (Test-Path $Path)) {
+        New-Item $Path -ItemType File
+    }
+    else {
+        (Get-ChildItem $Path).LastWriteTime = Get-Date
+    }
 }
+
